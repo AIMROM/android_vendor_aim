shagbag913/vendor_bootleggers-1

Adding more ringtones
and maybe another fix for zip stuff

config/common.mk
@@ -193,7 +193,7 @@ bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_siz
 define check_and_set_bootanimation
 $(eval TARGET_BOOTANIMATION_NAME := $(shell \
   if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
-    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
+    if [ $(1) -le $(TARGET_SCREEN_WIDTH) ]; then \
       echo $(1); \
       exit 0; \
     fi;
@@ -227,7 +227,7 @@ endif
 
 # Set all versions
 BOOTLEG_VERSION := BootleggersROM-$(PRODUCT_VERSION_MAJOR)4$(BOOTLEG_BUILD).$(PRODUCT_VERSION_MAINTENANCE).$(BOOTLEG_BUILD_TYPE)$(BOOTLEG_POSTFIX)
-BOOTLEG_MOD_VERSION := BootleggersROM-$(PRODUCT_VERSION_MAJOR)4$(BOOTLEG_BUILD).$(PRODUCT_VERSION_MAINTENANCE).$(BOOTLEG_BUILD_TYPE)-$(BOOTLEG_POSTFIX)
+BOOTLEG_MOD_VERSION := BootleggersROM-$(PRODUCT_VERSION_MAJOR)4$(BOOTLEG_BUILD).$(PRODUCT_VERSION_MAINTENANCE).$(BOOTLEG_BUILD_TYPE)$(BOOTLEG_POSTFIX)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
@@ -238,3 +238,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 EXTENDED_POST_PROCESS_PROPS := vendor/bootleggers/tools/bootleg_process_props.py
 
+#Call special ringtones makefile
+include vendor/bootleggers/config/common_audio.mk

config/common_audio.mk
@@ -1,0 +1,33 @@
+#
+# BootleggersROM Audio Files
+# based on CyanogenMod old one
+#
+
+ALARM_PATH := vendor/bootleggers/prebuilt/common/media/audio/alarms
+NOTIFICATION_PATH := vendor/bootleggers/prebuilt/common/media/audio/notifications
+RINGTONE_PATH := vendor/bootleggers/prebuilt/common/media/audio/ringtones
+
+# Alarms
+PRODUCT_COPY_FILES += \
+    $(ALARM_PATH)/BeyondTheDream.ogg:system/media/audio/alarms/BeyondTheDream.ogg \
+    $(ALARM_PATH)/LikeWhat.ogg:system/media/audio/alarms/LikeWhat.ogg
+
+# Notifications
+PRODUCT_COPY_FILES += \
+    $(NOTIFICATION_PATH)/Auroran.ogg:system/media/audio/notifications/Auroran.ogg \
+    $(NOTIFICATION_PATH)/CyanMail.ogg:system/media/audio/notifications/CyanMail.ogg \
+    $(NOTIFICATION_PATH)/CyanMessage.ogg:system/media/audio/notifications/CyanMessage.ogg \
+    $(NOTIFICATION_PATH)/Phloreau.ogg:system/media/audio/notifications/Phloreau.ogg
+
+# Ringtones
+PRODUCT_COPY_FILES += \
+    $(RINGTONE_PATH)/Caraway.ogg:system/media/audio/ringtones/Caraway.ogg \
+    $(RINGTONE_PATH)/Frost.ogg:system/media/audio/ringtones/Frost.ogg \
+    $(RINGTONE_PATH)/Myst.ogg:system/media/audio/ringtones/Myst.ogg
+    
+    
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Electra.ogg \
+    ro.config.alarm_alert=LikeWhat.ogg \
+    ro.config.ringtone=Frost.ogg

AIMROM/android_vendor_aim

Change-Id: I1f96ba928ed103d3b773df0dd30f8ac463fa3a28

config/common.mk
@@ -60,3 +60,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Fix Dialer
 PRODUCT_COPY_FILES +=  \
     vendor/aim/prebuilt/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml 
+
+#Call special ringtones makefile
+include vendor/aim/config/common_audio.mk

config/common_audio.mk
@@ -1,0 +1,33 @@
+#
+# aimROM Audio Files
+# based on CyanogenMod old one
+#
+
+ALARM_PATH := vendor/aim/prebuilt/common/media/audio/alarms
+NOTIFICATION_PATH := vendor/aim/prebuilt/common/media/audio/notifications
+RINGTONE_PATH := vendor/aim/prebuilt/common/media/audio/ringtones
+
+# Alarms
+PRODUCT_COPY_FILES += \
+    $(ALARM_PATH)/BeyondTheDream.ogg:system/media/audio/alarms/BeyondTheDream.ogg \
+    $(ALARM_PATH)/LikeWhat.ogg:system/media/audio/alarms/LikeWhat.ogg
+
+# Notifications
+PRODUCT_COPY_FILES += \
+    $(NOTIFICATION_PATH)/Auroran.ogg:system/media/audio/notifications/Auroran.ogg \
+    $(NOTIFICATION_PATH)/CyanMail.ogg:system/media/audio/notifications/CyanMail.ogg \
+    $(NOTIFICATION_PATH)/CyanMessage.ogg:system/media/audio/notifications/CyanMessage.ogg \
+    $(NOTIFICATION_PATH)/Phloreau.ogg:system/media/audio/notifications/Phloreau.ogg
+
+# Ringtones
+PRODUCT_COPY_FILES += \
+    $(RINGTONE_PATH)/Caraway.ogg:system/media/audio/ringtones/Caraway.ogg \
+    $(RINGTONE_PATH)/Frost.ogg:system/media/audio/ringtones/Frost.ogg \
+    $(RINGTONE_PATH)/Myst.ogg:system/media/audio/ringtones/Myst.ogg
+    
+    
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Electra.ogg \
+    ro.config.alarm_alert=LikeWhat.ogg \
+    ro.config.ringtone=Frost.ogg